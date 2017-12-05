a55cb728a99ddaee01858f9a851601e7120b2534
Joe Liao <joe83830@gmail.com>
2017-12-05 16:18:10 -0500

    unittest5

--- test.py
+++ test.py
@@ -1,12 +1,20 @@
 import unittest
+from main import app
 
 class IntegerArithmenticTestCase(unittest.TestCase):
+
     def testAdd(self):  ## test method names begin 'test*'
         self.assertEquals((1 + 2), 3)
         self.assertEquals(0 + 1, 1)
+
     def testMultiply(self):
         self.assertEquals((0 * 10), 0)
         self.assertEquals((5 * 8), 40)
+
+    def test_index(self):
+        tester = app.test_client(self)
+        response = tester.get('/')
+        self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
